88e5809a46a08375d4a8d0754f1f563e4d5d5917
Ben <up780065@myport.ac.uk>
2018-02-06 17:32:03 +0000

    Clock is now fully functional
    Now requires the DS3231 Real Time Clock Module.

--- Arduino code/Clock/Clock.ino
+++ Arduino code/Clock/Clock.ino
@@ -1,4 +1,8 @@
 #include "LedControl.h"
+#include <Wire.h>
+#include "DS3231.h"
+
+RTClib RTC;
 
 //Vars
 unsigned long delayFrame=100; //Frame rate
@@ -13,10 +17,9 @@ LedControl lc=LedControl(DIN,CLK,CS,displays);
 
 
 void setup() {
-  /*
-   * The MAX72XX is in power-saving mode on startup, we have to do a wakeup call
-   */
+  Wire.begin(); //Start RTC stuff
 
+  //The MAX72XX is in power-saving mode on startup, we have to do a wakeup call
   for(int i=0;i<displays;i++){
     lc.shutdown(i,false);
     // Set the brightness to a medium values 
@@ -27,13 +30,14 @@ void setup() {
   
 }
 
-void animate(int currentTime){
-
+void printNum(int disp, int num){
+  //Simply prints the number row by row to the display specified
+  
   //Numbers 0-9
   int frames = 10;
-  byte numbers[frames][8]=\
+  byte number[frames][8]=\
   {{B01111110,B01111110,B01100110,B01100110,B01100110,B01100110,B01111110,B01111110},\
-  {B00111000,B01111000,B01111000,B00011000,B00011000,B00011000,B00011000,B00011000},\
+  {B00011000,B00111000,B01111000,B00011000,B00011000,B00011000,B00011000,B00011000},\
   {B01111110,B01111110,B00000110,B00011110,B01111000,B01100000,B01111110,B01111110},\
   {B01111110,B01111110,B00000110,B00111110,B00111110,B00000110,B01111110,B01111110},\
   {B01100110,B01100110,B01111110,B01111110,B00000110,B00000110,B00000110,B00000110},\
@@ -43,50 +47,59 @@ void animate(int currentTime){
   {B01111110,B01111110,B01100110,B01111110,B01111110,B01100110,B01111110,B01111110},\
   {B01111110,B01111110,B01100110,B01111110,B01111110,B00000110,B00000110,B00000110}};
 
-  /* This is a mess
-   *  
-   *  h1/H1 here represent the furthest left digit of time, the first hour no,
-   *  h2/H2 is the second digit and the same with the minutes e.g.
-   *  0   2 : 3   5
-   *  h1  h2  m1  m2
-   */
-
-  for(int time=currentTime; time<720; time++){
-    //1 unit of time is 1 minute, 12*60=720
-    int hour = time/60; //int division, no remainder
-    int minute = time%60; //mod division, only remainder
+  for(int row=0;row<8;row++){lc.setRow(disp,row,number[num][row]);}
   
-    //Set first minute digit
-    for(int row=0;row<8;row++){
-      lc.setRow(0,row,numbers[(minute%10)][row]);
-    }
+}
 
-    //Set second minute digit
-    for(int row=0;row<8;row++){
-      lc.setRow(1,row,numbers[(minute/10)][row]);
-    }
+void animate(DateTime now){
+  /* This is a mess
+   * 
+   * The display is ordered as follows:
+   * [3][2][1][0]--cables
+   *  1  2 :5  6 
+   *  
+   * Print each digit individually because it's easier that way.
+   *  
+   * We work from right to left so the first digit (m2) is the second 
+   * digit of minutes, the second digit (m1) is the first digit of minutes,
+   * same goes for hours.
+   * 
+  */
+  int m2 = now.minute() % 10; //Only remainder
+  int m1 = now.minute() / 10; //No remainder
+  int h2 = now.hour() % 10;
+  int h1 = now.hour() / 10;
 
-    //Set hour digits
-    if(hour>9){
-      for(int row=0;row<8;row++){lc.setRow(3,row,numbers[1][row]);}
-      for(int row=0;row<8;row++){lc.setRow(2,row,numbers[(hour-10)][row]);};
-    }else{
-      for(int row=0;row<8;row++){lc.setRow(3,row,numbers[0][row]);};
-      for(int row=0;row<8;row++){lc.setRow(2,row,numbers[hour][row]);};
-    }
+  //Example time 12:56
+  
+  printNum(0,m2); // 6
+  printNum(1,m1); // 5
+  printNum(2,h2); // 2
+  printNum(3,h1); // 1
 
-    delay(60000);
-    
+  //fancy fading second animation
+  for(int i = 8; i>3; i--){
+    //Darken
+    lc.setIntensity(0,i);
+    lc.setIntensity(1,i);
+    delay(100);
   }
+  for(int i = 3; i<8; i++){
+    //Lighten
+    
+    //Minute displays
+    lc.setIntensity(0,i);
+    lc.setIntensity(1,i);
+    delay(100);
+  }
+
+  
 }
 
 void loop() { 
-  int h = 12;
-  int m = 4;
+  //Get current time
+  DateTime now = RTC.now();
 
-  int currentTime = (h*60)+m;
-  
-  //example:
-  //animate(11,50)
-  animate(currentTime);
+  //Pass it to the display animator
+  animate(now);
 }
